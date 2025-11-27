ab28342629f686e83c30f0871f9156fae92b42f5
karelyh <hernandezkarely729@gmail.com>
2025-11-26 21:54:15 -0600

    Arreglando ruta principal para Vercel

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// RUTA PRINCIPAL: Cuando entres a la web, devuelve el index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Conexión a Base de Datos MySQL
 // Railway nos dará la variable MYSQL_URL
