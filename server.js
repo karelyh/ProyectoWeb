58718ec53dd98fb7ea42bc7c9081f0ac6f02d497
karelyh <hernandezkarely729@gmail.com>
2025-12-04 22:00:56 -0600

    moviendo carpetas

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // RUTA PRINCIPAL: Cuando entres a la web, devuelve el index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Conexión a Base de Datos MySQL
